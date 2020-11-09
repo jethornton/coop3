5357edf2dad2b2b648c7675fb212315bf2139d56
john <bjt128@gmail.com>
2020-11-09 09:47:59 -0600

    initial commit

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # coop3
+Automated Chicken Coop Door and Lights using a Raspberry Pi 3 B
+
+Setup the Raspberry Pi 3 B per 
+[this tutorial](https://jethornton.github.io/rpi/install-10D.html)
+
+Copy `__init__.py, coop3, coop3.ui, and rpi_utilities.py` to your local bin directory.
+Make sure coop3 is executable, right click on it in the file manager and select
+properties.
+
+Additional notes are in the coop3 file.
---
+++ rpi_utilities.py
@@ -0,0 +1,85 @@
+import sys
+from time import monotonic_ns
+
+try:
+	import RPi.GPIO as gpio
+	gpio.setmode(gpio.BCM)
+	gpio.setwarnings(False)
+	OPEN = 0
+	CLOSED = 1
+	WAITING = 2
+except:
+	e = sys.exc_info()[0]
+	print(f'error is {e}')
+
+
+"""
+Call the update function to find out if the pin has changed
+
+To import the library
+from rpi_utilities import debounce
+
+In your __init__ function
+self.somePinName = debounce(pin=UP_PROX, delay=0.001)
+self.somePinState = 0
+
+In your fast running thread
+self.somePinState = self.somePinName.update()
+if self.somePinState != None:
+	print(f'Some Pine Value is {self.somePinState}')
+"""
+
+
+
+class debounce():
+	def __init__(self, *args, **kwargs):
+		if 'pin' in kwargs:
+			self.pin = kwargs.get('pin')
+			gpio.setup(self.pin, gpio.IN,pull_up_down=gpio.PUD_DOWN)
+			#print(f'Pin is {self.pin}')
+			if gpio.input(self.pin) == 0:
+				self.state = OPEN
+			if gpio.input(self.pin) == 1:
+				self.state = CLOSED
+			self.start = 0
+		if 'delay' in kwargs:
+			self.ns_delay = int(kwargs.get('delay') * 1000000000)
+		else:
+			self.ns_delay = int(0.1 * 1000000000)
+
+	def update(self):
+		pin_state = gpio.input(self.pin)
+		if self.state != pin_state and self.state != WAITING:
+			self.start = monotonic_ns()
+			self.state = WAITING
+			#print(f'Start {self.start}')
+		if self.state == WAITING:
+			now = monotonic_ns()
+			duration = now - self.start
+			#print(f'Duration {duration}')
+			if duration > self.ns_delay:
+				self.state = pin_state
+				#print(f'Pin {self.pin} New State is {self.state}')
+				return self.state
+
+	def current(self):
+		return gpio.input(self.pin)
+
+class ledfade():
+	def __init__(self, *args, **kwargs):
+		if 'start' in kwargs:
+			self.start = kwargs.get('start')
+		if 'end' in kwargs:
+			self.end = kwargs.get('end')
+		if 'action' in kwargs:
+			self.action = kwargs.get('action')
+		self.transit = self.end - self.start
+
+	def ledpwm(self, p):
+		return int(0.181+(0.0482*p)+(0.00323*p*p)+(0.0000629*p*p*p))
+
+	def update(self, now):
+		if self.action == 'sunrise':
+			return self.ledpwm(((now - self.start) / self.transit) * 100)
+		elif self.action == 'sunset':
+			return self.ledpwm(100 - ((now - self.start) / self.transit) * 100)
